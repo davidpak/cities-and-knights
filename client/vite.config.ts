a7e49d55502005265df961e9dbdd373bd28034b1
davidpak <sungh1404@gmail.com>
2025-05-03 00:53:46 -0700

    Changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Makes Vite available to other devices
+    port: 5173,       // Default Vite port
+  }
 })
+
+
+
